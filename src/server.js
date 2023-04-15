7ae15c037567eedbc4ad3a866e6b617059db27b1
Rodrigo <rodrigomartins.agro@gmail.com>
2023-04-15 10:37:29 -0300

    fix variável PORT

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 const app = require('./app');
 
 // não remova a variável `API_PORT` ou o `listen`
-const port = process.env.API_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log('ouvindo porta', port));

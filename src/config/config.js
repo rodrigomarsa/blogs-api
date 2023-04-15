2e70fcd9a70c58ced58749d87e50c8a7068adcf2
Rodrigo <rodrigomartins.agro@gmail.com>
2023-04-15 10:44:22 -0300

    fix variáveis de ambiente

--- src/config/config.js
+++ src/config/config.js
@@ -9,12 +9,12 @@ const suffix = {
 };
 
 const options = {
-  host: process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
+  host: process.env.MYSQLHOST || 'localhost',
+  port: process.env.MYSQLPORT || '3306',
   database: 
-    `${process.env.MYSQL_DB_NAME || 'blogs-api'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || '1234',
+    `${process.env.MYSQLDATABASE || 'blogs-api'}${suffix[environment] || suffix.test}`,
+  username: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || '1234',
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
